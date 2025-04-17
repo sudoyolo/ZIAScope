fc098c2ed0b71ccf6bdd9f20e45df1e065581d19
ShanyingLiu <jl200850984@gmail.com>
2025-04-17 12:35:16 -0400

    New commit for new selection script, trying to get scene in too

---
+++ Assets/SceneManipulation.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Text;
+using System;
+
+public class SceneManipulation: MonoBehaviour
+{
+    public GameObject sceneObjects;
+    public List<GameObject> rootChildren = new List<GameObject>();
+    [SerializeField] public Transform playerPos; 
+    [SerializeField] private Material selectedMat;
+    public List<GameObject> selectedObjects = new List<GameObject>();
+    public string result; 
+    
+    void Start()
+    {
+
+    }
+
+    public void SelectObject(string objIdx)
+    {
+        selectedObjects.Clear();
+
+        string[] parts = objIdx.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string part in parts)
+        {
+            if (int.TryParse(part, out int number))
+            {
+                selectedObjects.Add(rootChildren[number]);
+            }
+            else
+            {
+                Console.WriteLine($"Warning: Could not parse '{part}' as an integer.");
+            }
+        }
+
+        for (int i = 0; i<selectedObjects.Count; i++)
+        {
+            ChangeMaterial(selectedObjects[i], selectedMat);
+        }
+
+    }
+
+    public void ChangeMaterial(GameObject obj, Material newMat)
+    {
+        Renderer renderer = obj.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            renderer.material = newMat;
+        }
+    }
+}
